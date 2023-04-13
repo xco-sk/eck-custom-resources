8ed27dd8be233891634f52ff3c3be2c1097b9c27
Marek Hornak <marek@xco.sk>
2023-04-13 21:22:14 +0200

    #52 fix password encoding in elasticsearch user resource

--- utils/elasticsearch/user_utils.go
+++ utils/elasticsearch/user_utils.go
@@ -3,13 +3,14 @@ package elasticsearch
 import (
 	"context"
 	"encoding/json"
+	"strings"
+
 	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/xco-sk/eck-custom-resources/apis/es.eck/v1alpha1"
 	"github.com/xco-sk/eck-custom-resources/utils"
 	k8sv1 "k8s.io/api/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"strings"
 )
 
 func DeleteUser(esClient *elasticsearch.Client, userName string) (ctrl.Result, error) {
@@ -36,7 +37,7 @@ func UpsertUser(esClient *elasticsearch.Client, cli client.Client, ctx context.C
 		return ctrl.Result{}, unmarshallErr
 	}
 
-	userBody["password"] = password
+	userBody["password"] = string(password)
 	userWithPassword, marshallErr := json.Marshal(userBody)
 	if marshallErr != nil {
 		return ctrl.Result{}, marshallErr
