7c6e3794e61c029bd110284996a21690376aadd2
Marek Hornak <marek@xco.sk>
2023-09-03 14:45:32 +0200

    E2E tests setup

--- e2e/src/test/java/sk/xco/eckcr/steps/CommonStepDefinitions.java
+++ e2e/src/test/java/sk/xco/eckcr/steps/CommonStepDefinitions.java
@@ -14,6 +14,7 @@ import static sk.xco.eckcr.util.K8sClient.withK8sClient;
 public class CommonStepDefinitions {
 
     private static final String ECK_CR_POD_NAME = "eck-custom-resources-operator";
+    private static final String ES_POD_NAME_PATTERN = "%s-es";
 
     @Given("Kubernetes cluster is available")
     public void kubernetesClusterAvailable() {
@@ -32,6 +33,19 @@ public class CommonStepDefinitions {
                 fail("ECK-CR not installed");
             }
             log.info("ECK-CR present");
+            return null;
+        });
+    }
+
+    @And("Elasticsearch {string} is available")
+    public void elasticsearchAvailable(String esName) {
+        withK8sClient().run(client -> {
+            var pods = client.pods().inNamespace("default").list().getItems();
+            var esPodName = ES_POD_NAME_PATTERN.formatted(esName);
+            if (pods.stream().noneMatch(pod -> pod.getMetadata().getName().contains(esPodName))) {
+                fail("ES %s not installed".formatted(esName));
+            }
+            log.info("ES {} present", esName);
             return null;
         });
     }
--- e2e/src/test/resources/features/index.feature
+++ e2e/src/test/resources/features/index.feature
@@ -3,11 +3,17 @@ Feature: Kubernetes Resource Creation
   Background:
     Given Kubernetes cluster is available
     And ECK-CR operator is installed
+    And Elasticsearch "quickstart" is available
 
-  Scenario: Create a Pod
-    When I create a Pod with name "my-pod" and image "nginx"
-    Then the Pod "my-pod" should be running
+  Scenario: Create an Index
+    When the "index-test" is applied
+    Then the Index with name "test" with "0" replica shards is present in "quickstart" Elasticsearch
 
-  Scenario: Create a Service
-    When I create a Service with name "my-service" and type "ClusterIP"
-    Then the Service "my-service" should be available
+  Scenario: Update an Index
+    When the "index-modified" is applied
+    Then the Index with name "test" with "1" replica shards is present in "quickstart" Elasticsearch
+
+  Scenario: Delete an Index
+    When the "index-test" is applied
+    When the resource of type "index" with name "test" is deleted
+    Then the Index with name "test" is not present in "quickstart" Elasticsearch
